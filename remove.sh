e51cb37ad71c4de1716058ad1f8c0f03627ff577
Julio Sanchez <dunkel015@gmail.com>
2022-10-29 05:17:20 -0500

    feat:search comics

--- remove.sh
+++
@@ -1,4 +0,0 @@
-sudo docker volume prune -f
-sudo docker volume rm $(docker volume ls -q)
-sudo rm -rf mongo-volume app/__pycache__/ && mkdir mongo-volume
-sudo docker system prune -a -f && docker builder prune -a -f
